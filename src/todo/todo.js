cb9729feebd898116c7314a36bf6a7a5059e3fb0
Ahmed Hany <ahmedhanyhosny@outlook.com>
2022-02-17 20:23:18 +0200

    Add editBtn and editPriorityBtn buttons
    The editBtn button when clicked prompts the user for a new title and description. Also add an option to change priority when adding a new todo - Change todo element text color depending on wether the todo is complete or not - Change the element that holds a project from div to details

--- src/todo/todo.js
+++ src/todo/todo.js
@@ -5,7 +5,9 @@ function Todo(title, description, dueDate, priority, project = 'project title') 
     const proto = {
         markAsComplete: function() {
             this.complete = !this.complete;
-            console.log(this.complete);
+        },
+        changePriority: function(newPriority) {
+            this.priority = newPriority;
         },
     }
     const todoObj = Object.assign(Object.create(proto), obj);
@@ -15,21 +17,53 @@ function Todo(title, description, dueDate, priority, project = 'project title') 
 function todoView(todo) {
     const view = document.createElement('details');
     view.innerHTML = `<summary><p class="todo-title">${todo.title}</p><p>${todo.dueDate.getFullYear()}</p></summary>`;
-    view.innerHTML += `<p>${todo.description}</p><p>Priority: ${todo.priority}</p>`;
+    view.innerHTML += `<p class="todo-description">${todo.description}</p><p>Priority: <span class="priority">${todo.priority}</span></p>`;
 
     const markAsCompleteBtn = document.createElement('button');
-    markAsCompleteBtn.classList.add('mark-as-complete');
     markAsCompleteBtn.textContent = 'Mark as complete';
-    markAsCompleteBtn.addEventListener('click', todo.markAsComplete);
-    view.appendChild(markAsCompleteBtn);
+    markAsCompleteBtn.addEventListener('click', () => {
+        todo.markAsComplete();
+        if (todo.complete) {
+            view.style.color = 'green';
+            markAsCompleteBtn.textContent = 'Mark as incomplete';
+        } else {
+            view.style.color = 'black';
+            markAsCompleteBtn.textContent = 'Mark as complete';
+        }
+    });
 
+    const editBtn = document.createElement('button');
+    editBtn.textContent = 'Edit';
+    editBtn.addEventListener('click', () => {
+        const todoTitle = prompt('Enter todo title (Press enter to skip):', todo.title);
+        const todoDescription = prompt('Enter todo description (Press enter to skip):', todo.description);
+        todo.title = todoTitle;
+        todo.description = todoDescription;
+        view.querySelector('.todo-title').textContent = todoTitle;
+        view.querySelector('.todo-description').textContent = todoDescription;
+    });
+    
+    const editPriorityBtn = document.createElement('button');
+    editPriorityBtn.textContent = 'Change priority';
+    editPriorityBtn.addEventListener('click', () => {
+        let todoPriority = prompt('Enter a number from 1 through 3', todo.priority);
+        while (todoPriority < 1 || todoPriority > 3) {
+            todoPriority = prompt('Try again. Enter a number from 1 through 3', todo.priority);
+        }
+        todo.changePriority(todoPriority);
+        view.querySelector('.priority').textContent = todoPriority;
+    });
+    
     const removeTodoBtn = document.createElement('button');
-    removeTodoBtn.classList.add('remove-todo-btn');
     removeTodoBtn.textContent = 'Remove todo';
     removeTodoBtn.addEventListener('click', () => {
         projects[todo.project].removeTodo(todo.title);
         view.remove();
     });
+    
+    view.appendChild(markAsCompleteBtn);
+    view.appendChild(editBtn);
+    view.appendChild(editPriorityBtn);
     view.appendChild(removeTodoBtn);
 
     document.querySelector(`.project[data-id="${todo.project}"]`)
@@ -56,10 +90,10 @@ function Project(title) {
 }
 
 function projectView(project) {
-    const projectDiv = document.createElement('div');
+    const projectDiv = document.createElement('details');
     projectDiv.classList.add('project');
     projectDiv.setAttribute('data-id', project.title);
-    projectDiv.innerHTML = `<h1>${project.title}</h1>`;
+    projectDiv.innerHTML = `<summary><h1>${project.title}</h1></summary>`;
 
     const addTodoBtn = document.createElement('button');
     addTodoBtn.classList.add('add-todo-btn');
@@ -67,7 +101,11 @@ function projectView(project) {
     addTodoBtn.addEventListener('click', () => {
         const todoTitle = prompt('Enter todo title:', 'todo title');
         const todoDescription = prompt('Enter todo description:', 'todo desc');
-        const todo = Todo(todoTitle, todoDescription, new Date(), 1, project.title);
+        let todoPriority = prompt('Enter a number from 1 through 3', 1);
+        while (todoPriority < 1 || todoPriority > 3) {
+            todoPriority = prompt('Try again. Enter a number from 1 through 3', 1);
+        }
+        const todo = Todo(todoTitle, todoDescription, new Date(), todoPriority, project.title);
         project.addTodo(todo);
         todoView(todo);
     });
